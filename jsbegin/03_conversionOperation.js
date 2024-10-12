9333a59b02c78604127c51a0f47bc1075b16d0ec
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-10-12 10:18:22 +0000

    conversion of datatypes

---
+++ jsbegin/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score=null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33"=>33
+//"33ads"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn="hidkd"
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+//1=>true; 0=>false
+//""=>false; "jjdjs"=>true
+
+let someNo=33
+
+let stringNo=String(someNo)
+console.log(stringNo);  //33
+console.log(typeof stringNo);  //string
