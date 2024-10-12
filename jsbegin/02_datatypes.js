aa2343dc7d4b7350fbffbd936c487f834b7e40db
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-10-12 07:35:16 +0000

    datatypes

---
+++ jsbegin/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all code as newer version
+//alert(3+3) //we are using nodejs, not browser
+
+let name="Satyam"
+let age=18
+let isLogedIn= false
+let state;
+
+console.log(typeof age);
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object
