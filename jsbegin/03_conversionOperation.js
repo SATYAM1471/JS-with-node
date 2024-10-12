2e49eeedec176b99012a4c7a9c0d11eda59149d1
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-10-12 11:18:00 +0000

    conversion operation

--- jsbegin/03_conversionOperation.js
+++ jsbegin/03_conversionOperation.js
@@ -23,5 +23,36 @@ let booleanisLoggedIn=Boolean(isLoggedIn)
 let someNo=33
 
 let stringNo=String(someNo)
-console.log(stringNo);  //33
-console.log(typeof stringNo);  //string
+//console.log(stringNo);  //33
+//console.log(typeof stringNo);  //string
+
+
+/********************OPERATION************************** */
+
+
+let value=3
+let negValue= -value
+//console.log(negValue);
+
+let str1="hello"
+let str2=" satyam"
+
+let str3= str1+str2
+//console.log(str3);     //hello satyam
+// console.log("1" +2);  //12
+// console.log(1+"2");    //12
+// console.log("1"+2+2);    //122
+//console.log(1+2+"2");     //32
+
+
+// console.log(+true);          //1
+// console.log(+"");            //0
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameControl=100
+gameControl++;
+console.log(gameControl);
+
