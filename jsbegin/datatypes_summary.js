f8bff2d81202f14cf56eaa40b390cc17b93396e9
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-10-17 17:41:29 +0000

    memorytype

--- jsbegin/datatypes_summary.js
+++ jsbegin/datatypes_summary.js
@@ -10,5 +10,36 @@ const myFunction =function(){
     console.log("Hello World");
 }
 
-console.log(typeof score);  //number
-console.log(typeof myFunction); //function
+//console.log(typeof score);  //number
+//console.log(typeof myFunction); //function
+
+
+
+//++++++++++++++++++++++++++++++++++++
+
+//Stack(Primitive) ,Heap(Non-primitive)
+
+let myYoutubename = "satyamdotcom"
+
+let anothername=myYoutubename
+
+//console.log(anothername); //satyamdotcom
+
+
+anothername = "Pokemondotcom"
+
+
+console.log(myYoutubename);  //satyamdotcom
+console.log(anothername);  //Pokemondotcom
+
+let userOne ={
+    email: "user@google.com",
+    upi : "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "satyam@google.com"
+
+console.log(userOne.email);//satyam@google.com
+console.log(userTwo.email); //satyam@google.com
