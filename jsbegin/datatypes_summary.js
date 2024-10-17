ed5075e38be6253cd06addbc46876bc5a28aa68f
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-10-17 17:04:38 +0000

    datatype_summary

---
+++ jsbegin/datatypes_summary.js
@@ -0,0 +1,14 @@
+//const score=100;
+
+const heros =["shaktiman","naagraj","doga"];
+let myObj={
+    name:"satyam",
+    age: 22,
+}
+
+const myFunction =function(){
+    console.log("Hello World");
+}
+
+console.log(typeof score);  //number
+console.log(typeof myFunction); //function
