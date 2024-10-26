68663a290b8200a075195138cb36356074e91858
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-10-26 06:47:30 +0000

    numbers

---
+++ jsbegin/6_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8766
+
+//console.log(otherNumber.toPrecision(3));
+//here 3 indicates the number of digits it will give the output
+
+const hundreds =1000000 
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++ MATHS++++++++++++++++++++++//
+
+// console.log(Math);
+//  console.log(Math.abs(4));
+//  console.log(Math.ceil(4.2));
+//  console.log(Math.floor(4.9));
+
