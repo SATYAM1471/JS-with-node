c98b40df3ed684ce903800e9ba05a572fd51e5cf
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-10-26 05:00:48 +0000

    string

---
+++ jsbegin/5_strings.js
@@ -0,0 +1,38 @@
+const name ="satyam"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName= new String('satyam-Choudhary')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('5'));
+
+
+//console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)  //here we can not start from negative
+//console.log(newString);
+
+const anotherString = gameName.slice(-14,5)   //to get the data in reverse
+//console.log(anotherString);
+
+const newStringOne="  satyam  "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url ="https://satyam.com/satyam%21choudhary"
+
+//console.log(url.replace('%21','-'));
+
+//console.log(url.includes('%'));
+
+//console.log(gameName.split('-'));
