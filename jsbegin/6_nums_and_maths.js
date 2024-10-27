9d1af7dd6d09b9d4bda852e16e29af874d4e219e
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-10-27 05:23:20 +0000

    num and maths

--- jsbegin/6_nums_and_maths.js
+++ jsbegin/6_nums_and_maths.js
@@ -5,8 +5,8 @@ const score = 400
 const balance = new Number(100)
 //console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+//console.log(balance.toString().length);
+ //console.log(balance.toFixed(2));
 
 const otherNumber = 123.8766
 
@@ -25,12 +25,12 @@ const hundreds =1000000
 // console.log(Math.min(4,3,6,8));
 // console.log(Math.max(4,3,6,8));
 // console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
-const  min=10
-const max=20
+// const  min=10
+// const max=20
 
-console.log(Math.floor(Math.random() *(max-min +1))+ min);
+// console.log(Math.floor(Math.random() *(max-min +1))+ min);
 
 // remember the above formula
