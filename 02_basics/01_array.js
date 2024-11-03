96763e3865d57c7b8f52481498400e476de63f89
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-11-03 10:52:09 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,39 @@
+// array
+
+const myArr =[0,1,2,3,4,5]
+const myHeros=["shaktiman","batman"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)  // Add values in the array
+// myArr.pop() // Remove the last value
+// myArr.unshift(9)  //add the no. in 0th index of array
+//myArr.shift() // removes the 1st element of array
+
+//console.log(myArr.includes(9)); //give the o/p in true or false
+//console.log(myArr.indexOf(3));
+// give the o/p "-1" if the element is absent or index no. if element is there
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+//slice ,splice
+
+console.log("A", myArr);
+
+const myn1= myArr.slice(0,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2= myArr.splice(0,3)
+console.log("C",myArr);
+console.log(myn2);
