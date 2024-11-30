dcee98a7b9dfed3adb9d049291ebaa5652682c1b
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-11-30 17:46:14 +0000

    objectsecondpart

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -19,7 +19,39 @@ const regularUser = {
 
 const obj1 ={1:"a",2:"b"}
 const obj2 ={3:"a",4:"b"}
+const obj4 ={5:"a",6:"b"}
 
 // const obj3 ={obj1, obj2}
-Object.obj3=Object.assign(obj1,obj2)
-console.log(obj3);
+// const obj3=Object.assign({},obj1,obj2,obj4)
+
+const obj3={...obj1,...obj2} //mostly used syntax
+// console.log(obj3);
+
+
+const users =[
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+//VERY IMPORTANT
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
