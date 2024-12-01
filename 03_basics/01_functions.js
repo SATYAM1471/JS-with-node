abd375fccfb1f3d5a6e9fa2007e3a24d301f0457
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-12-01 12:52:43 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+function sayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("T");
+    console.log("Y");
+    console.log("A");
+    console.log("M");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+    
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result=number1 + number2
+    // return result
+    return number1 + number2
+}
+let  result = addTwoNumbers(3,4)
+
+console.log("Result: ",result);
