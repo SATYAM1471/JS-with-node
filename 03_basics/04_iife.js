af59f75755ceb9b9d96c4a6dc5d9a1415c89af67
Satyam Choudhary <153951580+SATYAM1471@users.noreply.github.com>
2024-12-21 18:19:08 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -6,7 +6,15 @@
 })();
 // IIFE is used to remove the pollution caused by global scope and to get the code executed smoothly
 
-( () => {
-    console.log(`DB CONNECTED TWO`);
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
     
-} )()
+} )('Satyam' )// name
+
+// sometimes ";" can be a reason for errror in the program.We can take the example of above two function
+const chai = () => {
+    let username="hitesh"
+    console.log(this.username);
+} 
+console.log(chai.username);
